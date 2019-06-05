d8bb78beb5302abcb4e0c54dee9f95c6486424e0
Sean Gilmore <gilmore@michael-sean.com>
2019-06-05 15:21:27 -0400

    Added Displaying a List and mock heroes file

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 10, name: 'Achilles' },
+    { id: 11, name: 'Acteon' },
+    { id: 12, name: 'Aeneas' },
+    { id: 13, name: 'Atlanta' },
+    { id: 14, name: 'Bellerophon' },
+    { id: 15, name: 'Dioscuri' },
+    { id: 16, name: 'Heracles' },
+    { id: 17, name: 'Jason' },
+    { id: 18, name: 'Meleager' },
+    { id: 19, name: 'Odysseus' },
+    { id: 20, name: 'Peleus' },
+    { id: 21, name: 'Perseus' },
+    { id: 22, name: 'Theseus' }
+];
